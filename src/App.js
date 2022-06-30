bf45340beccacd4ec8918765a06ef7b532289997
arifraj199 <karif9514@gmail.com>
2022-06-30 21:32:32 +0600

    responsive navbar design implement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import CompletedTask from './components/CompletedTask';
+import ToDo from './components/ToDo';
+import Calender from './components/Calender';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/completedtask' element={<CompletedTask></CompletedTask>}></Route>
+        <Route path='/todo' element={<ToDo></ToDo>}></Route>
+        <Route path='/calender' element={<Calender></Calender>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/CompletedTask.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CompletedTask = () => {
+    return (
+        <div>
+            <h2>Complete Task Page</h2>
+        </div>
+    );
+};
+
+export default CompletedTask;
